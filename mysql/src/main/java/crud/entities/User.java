8bfb1335357f2fd1c1ee7844db6e8942caecfad7
Shiraz Malkani <shmalkani09@gmail.com>
2019-12-03 12:51:40 +0500

    -- initial commit --
    fixed database connection error
    crud operations for user

---
+++ mysql/src/main/java/crud/entities/User.java
@@ -0,0 +1,58 @@
+package crud.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Table(name = "user")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", email=" + email + '}';
+    }
+}
